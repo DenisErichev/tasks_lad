8f278e58435c098953cecca9ffd4028fc77b5a1e
DenisErichev <erichev.denis.2003@mail.com>
2022-08-16 15:33:19 +0300

    Add task2

--- task2.js
+++ task2.js
@@ -1,1 +1,7 @@
-
+var formElement = document.forms['formElement'];
+formElement.addEventListener('focus', (evt)=>{
+    evt.target.classList.add('focused')
+},true)
+formElement.addEventListener('blur', (evt)=>{
+    evt.target.classList.remove('focused')
+},true)
