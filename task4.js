f997d504cffdbc2cc16888677e473f8976117365
DenisErichev <erichev.denis.2003@mail.com>
2022-08-18 18:06:41 +0300

    fix

--- task4.js
+++ task4.js
@@ -68,172 +68,99 @@ const player={
     ]
 }
 
- //функция выбора хода игроком
-const choicePlayerMove=(playerMoves,copyPlayerMoves)=>{ 
-    // проходим по всем ходам       
-    for(let i=0;i<copyPlayerMoves.moves.length;i++){  
-        //если cooldown первоначального объекта и копии равны то  
-        if(playerMoves.moves[i].cooldown === copyPlayerMoves.moves[i].cooldown){    
-            // выводим название доступного хода
-            console.log(`${i})`+ copyPlayerMoves.moves[i].name)     
-            for(let key in copyPlayerMoves.moves[i]){
-                 // выводим характеристики доступного хода
-                console.log(` ${key}: ${copyPlayerMoves.moves[i][key]}`) 
-            }
-            console.log('\n')
-        }
-    }
-     //ввод хода
-    let moveIndex = readlineSync.question("!!!!!Введите номер хода: "); 
-    //возвращаем номер хода в виде числа     
-    return +moveIndex       
-}
+//глобальная переменная
+let userHealth=0
+let blockedEnemyMoves={}
+let blockedUserMoves={}
 
-const generateRandomIndexMove=()=> Math.trunc(Math.random() * 3) 
-
-//функция которая увеличивает cooldown ходов на каждом ходе(если требуется)
-const addCooldownEveryRound=(playerMoves,copyPlayerMoves)=>{ 
-    //проходим по превоначальному объекту 
-    for(let i=0; i<playerMoves.length; i++){   
-        //если cooldown первоначального объекта и копии не равны                
-        if(playerMoves[i].cooldown !== copyPlayerMoves[i].cooldown ){ 
-            // увеличиваем cooldown копии
-            copyPlayerMoves[i].cooldown +=1 
-        }
-    }   
-}
-
-//выбор сложности(начальное здоровье Евстафия)
-const inputMaxHealth=()=>{      
-    let input=readlineSync.question("Введите maxHealth: ")
-    //проверка, что значение является числом
-    if(!isNaN(input)){
-
-        return input
-    }
-    else{
-
-        return inputMaxHealth()
+const choiceDifficulty=()=>{
+    userHealth = readlineSync.question("Введите здоровье Евстафия: "); 
+    if(isNaN(userHealth)){
+        console.log('Вы ввели не число!')
+        return choiceDifficulty()
     }
 }
 
-//функция создания копии объекта
-const makeCopyObject=(object)=>{
-    let copyObject = {...object}
-    //глубокое копирование свойств ходов первоначального объекта
-    copyObject.moves = object.moves.map(item => ({...item}))
+const getEnemyMove=(moves)=>{
+   const moveIndex= Math.trunc(Math.random()*moves.length)
+   return moves[moveIndex]
+}
+    
+const getAvailableMoves = (moves, blockedMoves)=>moves.filter(move=>!(Object.keys(blockedMoves).includes(move.name)))
 
-    return copyObject
+const changeCooldown=(blockedMoves)=>{
+    const changeBlockedMovesObj={}
+    Object.entries(blockedMoves).map(([key,value])=>[key,value-1])
+                                .filter(([_,value])=>value>=1)
+                                .forEach(([key,value])=>changeBlockedMovesObj[key]=value)
+    
+    return changeBlockedMovesObj
 }
 
-//Функция вывода текущих характеристик
-const showCharacteristicsDamage = (playerMoves,damage) =>{     
-    console.log(`      ~~~${playerMoves.name} maxHealth: ${playerMoves.maxHealth}~~~`)
-    console.log(`~~~Полученный damage от противника: ${damage}`)
+const getUserMoves=(moves)=>{
+    console.log('\nВам доступны ходы:')
+    moves.forEach((move,index)=>console.log(`${index+1})`+move.name))
+    moveIndex = readlineSync.question("Выберете ход: "); 
+    console.log('\n')
+    if(isNaN(moveIndex) || moveIndex>moves.length || moveIndex<=0){
+        console.log('Ошибка ввода.')
+        return getUserMoves(moves)
+    }
+    return moves[moveIndex-1]
 }
 
-const damageGeneration=(damageFromObject, objectToDamage)=>{
-    // Math.round(num * 100) / 100
-    console.log(damageFromObject)
-    console.log(objectToDamage)
-    let damage = ((damageFromObject.physicalDmg - (damageFromObject.physicalDmg * objectToDamage.physicArmorPercents)/100)+
-                 (damageFromObject.magicDmg - (damageFromObject.magicDmg * objectToDamage.magicArmorPercents)/100)).toFixed(2)
-
+const calculateDamage=(attack,block)=>{
+    const damage= (attack.physicalDmg-(attack.physicalDmg*block.physicArmorPercents)/100)+
+                  (attack.magicDmg-(attack.magicDmg*block.magicArmorPercents)/100)
     return damage
 }
-//функция убавляющая maxHealth, и показывающая урон
-const minusHealth = (playerObject, damage)=>{
-    playerObject.maxHealth -= damage
-    showCharacteristicsDamage(playerObject, damage)
+
+const getGameResult=()=>{
+    if(userHealth<=0 && monster.maxHealth<=0){
+        console.log('Ничья.')
+    }
+    else if(userHealth<=0){
+        console.log(`Победил ${monster.name}`)
+    }
+    else if(monster.maxHealth<=0){
+        console.log(`Победил ${player.name}`)
+    }
+    
 }
 
-const findMove=(originMove, copyMove ,indexMove, isUserMove)=>{
-    if(originMove.moves[indexMove].cooldown !== copyMove.moves[indexMove].cooldown && isUserMove){
-        console.log(`\n!!!!!Данный ход пока заморожен.!!!!!\n`)
-        return null
-    }
-    for(let i=0;i<originMove.moves.length; i++){ 
-        //если cooldown хода, первонач. объекта равен 0, и случайный индекс совпал 
-        if(originMove.moves[indexMove].cooldown === 0){   
-            //записываем кортеж из - копии хода и его индекса 
-            return {...originMove.moves[indexMove], indexMove}     
+const gameStart=()=>{
+    let countMove=0
+    choiceDifficulty()
+    while(userHealth>0 && monster.maxHealth>0){
+        countMove+=1
+        console.log(`Текущий ход: ${countMove}.`)
+
+        //Ход монстра
+        const monsterAvailableMoves=getAvailableMoves(monster.moves, blockedEnemyMoves)
+        const monsterCurrentMove=getEnemyMove(monsterAvailableMoves)
+        blockedEnemyMoves=changeCooldown(blockedEnemyMoves)
+        if(monsterCurrentMove.cooldown !== 0){
+            blockedEnemyMoves[monsterCurrentMove.name] = monsterCurrentMove.cooldown
+        }
+        console.log(`\nХод Лютого: ${monsterCurrentMove.name}`)
+
+        //Ход пользователя
+        const userAvailableMoves=getAvailableMoves(player.moves, blockedUserMoves)
+        const currentUserMove=getUserMoves(userAvailableMoves)
+        blockedUserMoves=changeCooldown(blockedUserMoves)
+        if(currentUserMove.cooldown!==0){
+            blockedUserMoves[currentUserMove.name] = currentUserMove.cooldown
         }
 
-        else if(i === indexMove && copyMove.moves[i].cooldown === originMove.moves[indexMove].cooldown){  //если индекс текущего хода и случаного равны, а также cooldown копии равен первонач.
-             // 'обнуляем' cooldown копии: делаем -1, т.к. увеличение cooldown использованных ходов происходит сразу на каждом следующем ходе,
-             // если поставим не -1, а 0, то ход будет доступен раньше
-            copyMove.moves[i].cooldown = -1     
-            //записываем кортеж из - копии хода и его индекса                                 
-            return {...originMove.moves[indexMove], indexMove}
-        
-        }
+        //Бой
+        let damageForUser=calculateDamage(monsterCurrentMove, currentUserMove)
+        let damageForMonster=calculateDamage(currentUserMove, monsterCurrentMove)
+        console.log(`Вы нанесли по монстру: ${damageForMonster} урона.`)
+        console.log(`Монстер нанес по Евстафию: ${damageForUser} урона.\n`)
+        userHealth = userHealth-damageForUser
+        monster.maxHealth = monster.maxHealth-damageForMonster
     }
-    return null
-}
-let copyMonster = makeCopyObject(monster)
-let user = makeCopyObject(player)
-//выбор сложности
-user.maxHealth = inputMaxHealth()    
-//счетчик текущего хода
-let countMove=0     
-while(copyMonster.maxHealth > 0 && user.maxHealth > 0){
-    countMove++ 
-    console.log("Текущий ход:",countMove)
-    //увеличиваем cooldown каждого хода
-    addCooldownEveryRound(monster.moves, copyMonster.moves)  
-     //увеличиваем cooldown каждого хода
-    addCooldownEveryRound(player.moves, user.moves)       
-
-    //Блок генерации хода монстра
-    // массив, в который будем заносить кортеж- копия хода и номер хода в объекте
-    let currentComputerMove = null       
-    while(!currentComputerMove){   
-        //случайный ход монстра
-        let indexMove = generateRandomIndexMove() 
-        currentComputerMove =findMove(monster,copyMonster,indexMove)
-    }
-    //Вывод характеристик случайного хода монстра
-    console.log(`Ходит ${copyMonster.name}: ${currentComputerMove.name}`)
-    monster.moves.map((item, index)=>{
-        if(index === currentComputerMove.indexMove){
-            for(let key in item){
-                console.log(` ${key}:`,item[key])
-            }
-        }
-    })
-    console.log('\n')
-    //
-
-    //Блок генерации хода игрока
-    let currentUserMove = null
-    while(!currentUserMove){
-        //выбор хода пользователем
-        let userIndexMove=choicePlayerMove(player,user)
-        currentUserMove=findMove(player, user, userIndexMove,true)
-    }
-    console.log(`\n${user.name} ход: ${currentUserMove.name}\n`)
-
-    //урон от монстра
-
-    let damageFromMonster = damageGeneration(currentComputerMove, currentUserMove,copyMonster)
-    console.log()
-    //урон от пользователя
-    let damageFromUser = damageGeneration(currentUserMove, currentComputerMove, user)
-    minusHealth(copyMonster, damageFromUser)
-    minusHealth(user, damageFromMonster)
-
+    getGameResult()
 }
 
-//Блок проверки на выигрышь/проигрышь/ничью
-if(user.maxHealth <= 0){        
-    console.log(`\nПобедил: ${copyMonster.name}.`)
-    console.log(`${user.name} Health: ${user.maxHealth}`)
-}
-else if(copyMonster.maxHealth <= 0){
-    console.log(`\nПобедил: ${user.name}.`)
-    console.log(`${copyMonster.name} health: ${copyMonster.maxHealth}`)
-}
-else if(copyMonster.maxHealth <= 0 && user.maxHealth <= 0){
-    console.log('Ничья!')
-}
-//
+gameStart()
